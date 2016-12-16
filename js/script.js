c2a4c035934aa34e24d69de668a2e6853e965561
0smar <osmar.roramas@gmail.com>
2016-12-16 16:05:11 -0600

    v1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal();
+sr.reveal('.logo');
+sr.reveal('.slogan');
+sr.reveal('.description');
+sr.reveal('.graphi');
+sr.reveal('.circlei');
+sr.reveal('.moo');
+sr.reveal('.form');
+sr.reveal('.fimg');
